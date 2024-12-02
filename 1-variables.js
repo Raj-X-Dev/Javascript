97ded8064b4f419c2442d86ae3e165cbf1ea3383
Raj-X-Dev <mandalrajji123@gmail.com>
2024-12-02 22:52:45 +0530

    dataTypes

---
+++ 1-variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mandalrajji123@gmail.com"
+var accountPassword = "12344"
+accoutCity = "Delhi"
+let accountState;
+
+//accountId = 4444
+accountEmail = "raj@gmail.com"
+accountPassword = "1122"
+accoutCity = "haryana"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accoutCity,accountState])
---
+++ 2-dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we afe using node.js,not browser
+
+console.log("Hitesh");
+
+let name="raj"
+let age=20
+let isLoggedIn=false
+
