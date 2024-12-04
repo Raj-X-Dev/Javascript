d7d755990120762c9c32b08bbc8b45ce84f6f8f7
Raj-X-Dev <mandalrajji123@gmail.com>
2024-12-04 22:06:53 +0530

    Lesson-5

---
+++ 3_4-coversionOperation.js
@@ -0,0 +1,64 @@
+let score = "Raj"
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+// value > 0 => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************Operation****************/
+
+let Val = 3
+let negVal = -Val
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log(true);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+// console.log("2" == 2);
+
---
+++ dataTypes-summary.js
@@ -0,0 +1,54 @@
+// Primitive
+
+// 7 types : String, Numver, Boolean, Null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId =Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3694617861468974019n
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function () {
+//     console.log("Hello world");
+// }
+
+// console.log(typeof bigNumber);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "hiteshChannel"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let user = {
+    email: "user@gmail.com",
+    upi: "upi@1223"
+}
