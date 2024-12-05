3f00c5a04db08f0991e76149db1c2b619f43a812
Raj-X-Dev <mandalrajji123@gmail.com>
2024-12-05 22:53:08 +0530

    DateInJS

---
+++ 7-dateInJS.js
@@ -0,0 +1,26 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getTimezoneOffset());
+
+let myCreatedDate = new Date(2024,1,5)
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
