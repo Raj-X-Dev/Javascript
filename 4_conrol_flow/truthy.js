99ae491f1b340248291d0d81a5aeeb053f062d06
Raj-X-Dev <mandalrajji123@gmail.com>
2024-12-17 22:14:53 +0530

    45_min

---
+++ 4_conrol_flow/truthy.js
@@ -0,0 +1,16 @@
+// const userEmail = "h@hitesh.ai"
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+
+// true, "0", "false", " ", [], {}, function(){}, =
